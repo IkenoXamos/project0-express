9a9cd2fbe2ca6e058b8af24d1b54cf1e7e949232
unknown <moberlies1337@gmail.com>
2020-06-07 00:08:39 -0700

    Changed structure to include App class

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+
+export default class App {
+  public port: number;
+  public app: express.Application;
+
+  constructor(controllers: Array<any>, port: number) {
+    this.app = express();
+    this.port = port;
+
+    this.initializeMiddleware();
+    this.initializeControllers(controllers);
+
+    this.app.post('/login', (req, res) => {
+      const { username, password } = req.body;
+
+      console.log(`${username} tried to login with password ${password}`);
+
+      res.status(200).send(`${username} has successfully logged in`);
+      res.status(200).send();
+    });
+  }
+
+  private initializeMiddleware(): void {
+    this.app.use(bodyparser.json());
+  }
+
+  private initializeControllers(controllers: Array<any>): void {
+    controllers.forEach( (controller) => {
+      this.app.use('/', controller.router);
+    });
+  }
+
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`App has started listening on port ${this.port}`);
+    })
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import express from 'express';
-import bodyparser from 'body-parser';
+import App from 'app';
 
-const PORT = 9001;
-const app = express();
+const app = new App([], 9001);
 
-app.use(bodyparser.json());
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  console.log(`${username} tried to login with password ${password}`);
-
-  res.status(200).send(`${username} has successfully logged in`);
-  res.status(200).send();
-});
-
-app.listen(PORT, () => {
-  console.log(`App has started listening on port ${PORT}`);
-});
+app.listen();
