57f698c0e073bf629df3e0fb5ae6ed5301640489
unknown <moberlies1337@gmail.com>
2020-06-07 18:02:31 -0700

    Changed UserController to leverage a service

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,20 +1,22 @@
 import { Request, Response, Router } from 'express';
 import Controller from './controller.interface';
+import UserService from '../services/user.service';
 
 export default class UserController implements Controller {
   public readonly path: string = '/user';
   public readonly router: Router = Router();
+  private readonly service: UserService = new UserService();
 
   public initializeRoutes() {
     this.router.get(this.path, this.getAllUsers);
     this.router.post(this.path, this.createUser);
   }
 
-  public getAllUsers = (req: Request, res: Response) => {
-    res.send();
+  public getAllUsers = (_req: Request, res: Response): void => {
+    res.send(this.service.getAllUsers());
   }
 
-  public createUser = (req: Request, res: Response) => {
+  public createUser = (_req: Request, res: Response) => {
     res.send();
   }
 }
