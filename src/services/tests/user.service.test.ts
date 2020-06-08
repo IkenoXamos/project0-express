165f827cbfad42e9915dbb36d445d53e2e858822
unknown <moberlies1337@gmail.com>
2020-06-07 18:03:51 -0700

    Added first test for UserService and updated test script to use Jest

---
+++ src/services/tests/user.service.test.ts
@@ -0,0 +1,21 @@
+import UserService from "../../services/user.service";
+import { User } from "../../models/user";
+
+describe('The UserService', () => {
+  const service: UserService = new UserService();
+
+  describe('when obtaining all users', () => {
+    const result: any = service.getAllUsers();
+    it('should return an array', () => {
+      expect(Array.isArray(result))
+        .toBeTruthy();
+    });
+
+    it('should contain Users', () => {
+      if(result.length > 0) {
+        expect(result[0] instanceof User)
+          .toBeTruthy();
+      }
+    });
+  });
+});
