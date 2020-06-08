71b20e31cdff3b5c2a2d584cf3eeb60033e47792
unknown <moberlies1337@gmail.com>
2020-06-07 17:37:54 -0700

    Created Controller interface

---
+++ src/controllers/controller.interface.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+
+export default interface Controller {
+  readonly path: string;
+  readonly router: Router;
+}
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
+import Controller from './controller.interface';
 
-export default class UserController {
+export default class UserController implements Controller {
   public readonly path: string = '/user';
   public readonly router: Router = Router();
 
